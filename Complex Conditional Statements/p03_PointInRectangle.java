b238532805c29976f823d6c8d5d8db07059c9815
NikolayKaradzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2018-02-08 13:52:52 +0200

    Create p03_PointInRectangle.java

---
+++ Complex Conditional Statements/p03_PointInRectangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class p03_PointInRectangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double x1 = Double.parseDouble(scanner.nextLine());
+        double y1 = Double.parseDouble(scanner.nextLine());
+        double x2 = Double.parseDouble(scanner.nextLine());
+        double y2 = Double.parseDouble(scanner.nextLine());
+
+        double x = Double.parseDouble(scanner.nextLine());
+        double y = Double.parseDouble(scanner.nextLine());
+
+        if (x >= x1 && x <= x2 && y >= y1 && y <= y2) {
+            System.out.println("Inside");
+        } else {
+            System.out.println("Outside");
+        }
+    }
+}
