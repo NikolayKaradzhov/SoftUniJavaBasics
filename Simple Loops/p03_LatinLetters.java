5dd6ab5a9c869977051ae097d78dd775e9d6f3f4
NikolayKaradzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2018-02-08 14:02:54 +0200

    Create p03_LatinLetters.java

---
+++ Simple Loops/p03_LatinLetters.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class p03_LatinLetters {
+    public static void main(String[] args) {
+
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
