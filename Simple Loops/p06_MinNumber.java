7a23f1a4839a4920f6c5ad1f096f4d8f1de37a74
NikolayKaradzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2018-02-08 14:04:52 +0200

    Create p06_MinNumber.java

---
+++ Simple Loops/p06_MinNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class p06_MinNumber{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int maxNum = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+            int num = Integer.parseInt(scanner.nextLine());
+            if (num < maxNum) {
+                maxNum = num;
+            }
+        }
+        System.out.println(maxNum);
+    }
+}
