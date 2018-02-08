9375c006de928bf8b0333c2560fb21307a97e5ad
NikolayKaradzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2018-02-08 11:51:43 +0200

    Update and rename p01_Expression.java to p02_Expression.java

--- FirstStepsInCoding/p02_Expression.java
+++ FirstStepsInCoding/p02_Expression.java
@@ -1,4 +1,4 @@
-public class p01_Expression {
+public class p02_Expression {
     public static void main(String[] args) {
         System.out.println(((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3));
     }
